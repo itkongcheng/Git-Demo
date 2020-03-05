551e06f8e4b9b2ae7c23e984aa00e8c5eea86278
itkongcheng <it_cheng@qq.com>
2020-03-05 15:58:51 +0800

    尝试进行第二次更改

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -3,6 +3,8 @@ package com.duyi;
 public class MainTest {
 
     public static void main(String[] args) {
-        System.out.println("666");
+        System.out.println("你牛逼啊");
+        System.out.println("你天秀");
     }
+
 }
